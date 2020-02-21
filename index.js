564d726c4098827167aacf98cbae1f81dd3fe550
Jan Sieler <jan@sieler.im>
2020-02-21 12:01:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,40 +7,39 @@ class hexaone {
 
   /* API ENDPOINTS */
   async getCurrencies(currency_code) {
-    return await this.doRequest(`currencies/${currency_code}`);
+    return await this.doRequest(`market/currencies/${currency_code}`);
   }
 
   async getItems(app_id) {
-    return await this.doRequest(`items/${app_id}`);
-  }
-
-  async getPrices(app_id) {
-    return await this.doRequest(`prices/${app_id}`);
+    return await this.doRequest(`market/items/${app_id}`);
   }
 
   async getListings(app_id) {
-    return await this.doRequest(`listings/${app_id}`);
+    return await this.doRequest(`market/listings/${app_id}`);
+  }
+
+  async getPrices(app_id) {
+    return await this.doRequest(`market/prices/${app_id}`);
   }
 
   async getProfile(steam_id) {
-    return await this.doRequest(`profile/${steam_id}`);
+    return await this.doRequest(`user/profile/${steam_id}`);
   }
 
   async getInventory(steam_id, app_id, context_id) {
-    return await this.doRequest(`inventory/${steam_id}/${app_id}/${context_id}`);
+    return await this.doRequest(`user/inventory/${steam_id}/${app_id}/${context_id}`);
   }
 
   /* DEFAULT REQUEST HELPER */
   async doRequest(endpoint) {
-    const response = await axios({
+    const res = await axios({
       url: `https://api.hexa.one/${endpoint}?key=${this.key}`,
       method: 'GET',
       headers: {
         'User-Agent': 'HEXA.ONE NodeJS Package'
       }
     });
-
-    return response.data;
+    return res.data;
   }
 }
 
